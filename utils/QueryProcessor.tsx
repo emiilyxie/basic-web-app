cf0039c3f5448e9024873d8819781a35a1721a00
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2025-02-25 09:56:37 -0500

    add numbers query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -15,5 +15,12 @@ export default function QueryProcessor(query: string): string {
     return "elx";
   }
 
+  if (query.toLowerCase().includes("numbers")) {
+    const numbers = query.match(/\d+/g)?.map(Number);
+    if (numbers && numbers.length > 0) {
+      return Math.max(...numbers).toString();
+    }
+  }
+
   return "";
 }
