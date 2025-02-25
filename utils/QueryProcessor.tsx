f3df7dfbdad0e75c92a6e02a560d2737ec90d87e
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2025-02-25 09:57:31 -0500

    add addition query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -22,5 +22,12 @@ export default function QueryProcessor(query: string): string {
     }
   }
 
+  if (query.toLowerCase().includes("plus")) {
+    const numbers = query.match(/\d+/g)?.map(Number);
+    if (numbers && numbers.length == 2) {
+      return (numbers[0] + numbers[1]).toString();
+    }
+  }
+
   return "";
 }
